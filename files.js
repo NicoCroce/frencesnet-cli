9f9c40741f2704c0c2af091275ba1a4d693eca4d
Nicolás Croce <ingcrocenicolas@outlook.com>
2018-05-13 03:37:33 -0300

    - Se agregó la consola interactiva. - Se crearon promesas para saber cuando estaban crreados los archivos.

--- files.js
+++ files.js
@@ -1,31 +1,41 @@
 'use strict';
 
-(()=>{
-    const   fs = require('fs'),
-            utilFiles = require('./utilFiles.js');
-    
+(() => {
+    const fs = require('fs'),
+        utilFiles = require('./utilFiles.js');
+
     class file {
         createComponent(variable) {
-            if (fs.existsSync(variable)) {
-                return console.log(' EL COMPONENTE YA EXISTE! ')
-            } else {
-                fs.mkdirSync(variable);
-            }
-
-            createFile(variable, 'scss', utilFiles.getCssData(variable));
-            createFile(variable, 'html', utilFiles.getHTMLdata(variable));
-            createFile(variable, 'js', utilFiles.getJsData(variable));
+            return new Promise((resolve, reject) => {
+                if (fs.existsSync(variable)) {
+                    console.log(' EL COMPONENTE YA EXISTE! ');
+                    reject();
+                } else {
+                    fs.mkdirSync(variable);
+                    
+                    Promise.all([createFile(variable, 'scss', utilFiles.getCssData(variable)),
+                    createFile(variable, 'html', utilFiles.getHTMLdata(variable)),
+                    createFile(variable, 'js', utilFiles.getJsData(variable))])
+                    .then(() => {
+                        resolve();
+                    })
+                }
+            });
         }
     }
 
     let createFile = (variable, extension, data) => {
-        var fileName = variable + '/' + variable + '.' + extension;
-        fs.appendFile(fileName, data, function (err) {
-            if (err) throw err;
-            console.log('Saved!  ' + extension);
+        return new Promise((resolve, reject) => {
+            var fileName = variable + '/' + variable + '.' + extension;
+            fs.appendFile(fileName, data, function (err) {
+                if (err) throw err;
+            }, () => {
+                console.log('✔️   ' + variable + '.' + extension);
+                resolve();
+            });
         });
     }
-    
+
     module.exports = file;
 
 })();
--- index.js
+++ index.js
@@ -1,26 +1,70 @@
 #!/usr/bin/env node
 'use strict';
 
-    const   file = require('./files.js'),
-            program = require('commander');
-            
-    let createFile = new file();
+const file = require('./files.js'),
+    inquirer = require('inquirer'),
+    program = require('commander');
+
+let createFile = new file();
+
+
+// HEADER
+process.stdout.write('\x1Bc');
+
+console.log('🔥  🔥  🔥    Frances Net CLI  Desarrollado por Nicolás Croce --> ingcrocenicolas@outlook.com 😉  🍺');
+console.log('\n');
 
 program
-    .version('0.0.1', '-v, --version')
+    .version('1.0.9', '-v, --version')
     .description('Crear componente para fnet');
 
-program
-    .command('fnet <variable>')
-    .alias('nc')
-    .description('   Crear componente para fnet')
-    .action((variable) => {
-        createFile.createComponent(variable);
+
+let actions = {
+    1: createComponenet,
+    2: createFactory
+}
+
+
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: "accion",
+            message: '¿QUÉ QUIERES HACER?',
+            choices: [
+                '1 - Crear Componente',
+                '2 - Crear Factory',
+                new inquirer.Separator()
+            ]
+        },
+        {
+            type: 'input',
+            name: 'nombre',
+            message: '¿Qué nombre deseas utilizar?'
+        }
+    ])
+    .then(answers => {
+        console.log('\n');
+        actions[answers.accion.charAt(0)](answers.nombre); //Obtiene el número de la acción
     });
 
 
+function createComponenet(variable) {
+    createFile.createComponent(variable)
+        .then((res) => {
+            console.log('\n');
+            console.log('👌 ¡Componente creado con éxito! 🎉');
+        });
+}
+
+function createFactory(variable) {
+    console.log('Crea factoey');
+}
+
+
+/*
 program.on('--help', function () {
     console.log('fnet newComponent <Nombre>');
-});
+}); */
 
 program.parse(process.argv);
